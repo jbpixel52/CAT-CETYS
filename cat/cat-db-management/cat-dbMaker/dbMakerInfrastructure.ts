1ec640e799716190289dec6bdfb7f636c406ed05
Omar Duran <54329113+OktaOEDC@users.noreply.github.com>
2022-09-03 00:06:02 -0700

    Creador de campos de templetes

--- cat/cat-db-management/cat-dbMaker/dbMakerInfrastructure.ts
+++ cat/cat-db-management/cat-dbMaker/dbMakerInfrastructure.ts
@@ -4,229 +4,47 @@ import { notNull } from 'jest-mock-extended';
 
 const prisma = new PrismaClient()
 const noValueGlobal: string = "NONE";
-
-
-async function makeEquivalenceTable() {
-    // Connect the client
-    await prisma.$connect()
-    console.log('Connected to database');
-
-    await prisma.globales_Cartas.create({
-      data:
-      {
-          NOMBRE_MATERIA:{
-            CETYS: "Nombre de la asignatura",
-            CASEI: "2. Nombre del curso",
-            ABET:  "1. Course number and name"
-          },
-          REQUISITOS_MATERIA:{
-            CETYS: "Seriación/Prerrequisito",
-            CASEI: "3. Seriación o prerrequisitos",
-            ABET: noValueGlobal
-          },
-          NOMBRE_CARRERA:{
-            CETYS: "PROGRAMA ACADÉMICO",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          CLAVE_MATERIA:{
-            CETYS: "Clave",
-            CASEI: "1. Clave del curso",
-            ABET:  "1. Course number and name"
-          },
-          SEMESTRE:{
-            CETYS: "Periodo/Ciclo",
-            CASEI: "4. Ubicación (periodo en que se imparte)",
-            ABET: noValueGlobal
-          },
-          NIVEL_ACADEMICO:{
-            CETYS: "Línea curricular",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          TIPO_CURSO:{
-            CETYS: "Tipo",
-            CASEI: "5. Tipo de curso",
-            ABET: noValueGlobal
-          },
-          EJES:{
-            CETYS: [noValueGlobal],
-            CASEI: ["Ciencias Básicas", "Ciencias de la Ingeniería", "Ingeniería Aplicada",
-            "Diseño en Ingeniería", "C. Sociales y Humanidades", "C. Económ. Administrat", "Otros Cursos"],
-            ABET: [noValueGlobal]
-          },
-          MODALIDAD:{
-            CETYS: "Modalidad: ",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          IDIOMA:{
-            CETYS: "Idioma de impartición",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          CREDITOS:{
-            CETYS: "Créditos (CR)",
-            CASEI: noValueGlobal,
-            ABET: "2.	Credits, contact hours, and categorization of credits"
-          },
-          INSTALACIONES:{
-            CETYS: "Instalaciones",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE:{
-            CETYS: "Número de horas con docente al semestre (HDT+HDP)",
-            CASEI: "6. Horas totales",
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE_SEMANALES:{
-            CETYS: "Número de horas teóricas y prácticas con docente semanal (HTs y  HPs)",
-            CASEI: "10.a Horas a la semana",
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE_INDEPENDIENTES:{
-            CETYS: "Número de horas de estudio independiente al semestre (HI)",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE_INDEPENDIENTES_SEMANALES:{
-            CETYS: "Número de horas de estudio independiente semanal (HIs)",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE_TOTALES:{
-            CETYS: "Número total de horas al semestre HT (HDT+HDP+HI)",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          HORAS_CLASE_TOTALES_SEMANALES:{
-            CETYS: "Total de horas a la semana (HTs+HPs+HIs)",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          OBJETIVOS_CURSO:{
-            CETYS: "Propósito general del curso",
-            CASEI: "7. Objetivos del curso \n Principales resultados de aprendizaje (indicadores de los AE)",
-            ABET:  "6. Specific goals for the course"
-          },
-          COMPETENCIAS:{
-            CETYS: {
-              GENERAL: "Competencia general de la asignatura",
-              UNIDAD: "Competencias de la unidad"
-            },
-            CASEI: noValueGlobal,
-            ABET:  noValueGlobal
-          },
-          METODOS_ACTIVIDADES:{
-            CETYS: "Métodos y actividades de aprendizaje bajo conducción del docente",
-            CASEI: noValueGlobal,
-            ABET:  noValueGlobal
-          },
-          ACTIVIDADES_INDEPENDIENTES:{
-            CETYS: "Actividades de aprendizaje independientes ",
-            CASEI: noValueGlobal,
-            ABET:  noValueGlobal
-          },
-          METODOLOGIA_EVALUACION:{
-            CETYS: "Metodología de evaluación",
-            CASEI: noValueGlobal,
-            ABET:  noValueGlobal
-          },
-          INSTRUMENTOS_MEDICION_APRENDIZAJE:{
-            CETYS: "Instrumentos para la medición del aprendizaje",
-            CASEI: noValueGlobal,
-            ABET:  noValueGlobal
-          },
-          APORTACION_CURSO_CACEI:{
-            CETYS: noValueGlobal,
-            CASEI: "9. Aportación del curso a los atributos de egreso del CACEI",
-            ABET: noValueGlobal
-          },
-          APORTACION_CURSO_PE:{
-            CETYS: noValueGlobal,
-            CASEI: "8. Aportación del curso a los atributos de egreso del PE",
-            ABET: noValueGlobal
-          },
-          DATOS_RELEVANTES_CURSO:{
-            CETYS: "Datps CETYS",
-            CASEI: "10. Datos relevantes del curso",
-            ABET: noValueGlobal
-          },
-          INFORMACION_ESPECIFICA:{
-            CETYS: noValueGlobal,
-            CASEI: noValueGlobal,
-            ABET: "5.	Specific course information"
-          },
-          EVIDENCIAS:{
-            CETYS: "Evidencias de desempeño",
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          UNIDAD:{
-            CETYS: "Unidad de competencia ",
-            CASEI: "",
-            ABET:  ""
-          },
-          TEMAS_CURSO:{
-            CETYS: "Contenidos curriculares",
-            CASEI: "11. Contenido sintético del curso",
-            ABET:  "7. Brief list of topics to be covered"
-          },
-          ESTRATEGIAS_ENSENANZA:{
-            CETYS: "Estrategias CETYS",
-            CASEI: "12. Principales estrategias de enseñanza",
-            ABET: noValueGlobal
-          },
-          ESTRATEGIAS_EVALUACION:{
-            CETYS: "Estrategias CETYS",
-            CASEI: "13. Principales estrategias de evaluación",
-            ABET: noValueGlobal
-          },
-          PRACTICAS:{
-            CETYS: noValueGlobal,
-            CASEI: "14. Prácticas",
-            ABET: noValueGlobal
-          },
-          RETROALIMENTACION:{
-            CETYS: noValueGlobal,
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          CRITERIOS_EVALUACION:{
-            CETYS: noValueGlobal,
-            CASEI: noValueGlobal,
-            ABET: noValueGlobal
-          },
-          BIBLIOGRAFIA:{
-            CETYS: {
-              BASICA: "Bibliografía básica",
-              COMPLEMENTARIA: "Bibliografía complementaria",
-              ELECTRONICOS: "Recursos electrónicos",
-              APOYOS: "Apoyos y recursos"
-            },
-            CASEI: "15. Bibliografía",
-            ABET:   "4. Text book, title, author, and year"
-          },
-          PROFESORADO:{
-            CETYS: "Perfil del docente",
-            CASEI: "16. Profesores",
-            ABET:  "3.	Instructor's or course coordinator's name"
-          },
-      }
-    })
+const enum fieldTypes {
+  soloTexto = "SOLO_TEXTO",
+  simple = "1 a 1",
+  oneToList = "1 a lista",
+  oneToTable = "1 a tabla",
+  selectionWOName = "Seleccion s/nombre",
+  selectionWName = "Seleccion c/nombre",
+  sectionTitle = "Titulo seccion"
 }
 
 
-makeEquivalenceTable()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+async function MakeField(){
+      // Connect the client
+      await prisma.$connect()
+      console.log('Connected to database');
+  
+      await prisma.camposBase.create({
+        data:
+        {
+          NOMBRE_CAMPO: {
+            CETYS: "Contenidos curriculares",
+            CACEI: "",
+            WASC: "",
+            ABET: ""
+          },
+          DESCRIPCION_CAMPO: "Temas de la unidad",
+          TIPO_CAMPO: fieldTypes.oneToList,
+          OPCIONES_SELECCION: [],
+          NOMBRES_OPCIONES_SELECCION: []
+        }
+      });
+}
 
-console.log('Finished writing equivalence table');  
+MakeField()
+.then(async () => {
+  await prisma.$disconnect()
+})
+.catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
+
 export default prisma
