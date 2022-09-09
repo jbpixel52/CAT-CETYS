37ff845fe1907c3d7f08b0d6fab05f8c8dfab05f
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-08 18:31:39 -0700

    editor page edited :)

--- cat/components/login.tsx
+++ cat/components/login.tsx
@@ -9,14 +9,7 @@ export default function Login() {
   if (session) {
     //if logged in return user info [name, email and profile photo] and show the option to log out
     return (
-      <>
-        <p>Signed in as {session?.user?.name}</p>
-        <p>Email is {session?.user?.email}</p>
-        {/* The  !  post-fix expression operator may be used to assert that its operand is non-null and non-undefined in contexts where the type checker is unable to conclude that fact.*/ }
-        <br />
-        <Button variant="outlined" onClick={() => signOut()}>Sign Out</Button>
-      </>
-    )
+      <Button variant="outlined" onClick={() => signOut()}>Sign Out</Button>)
   } else {
 
     return (
--- cat/pages/desk.tsx
+++ cat/pages/desk.tsx
@@ -1,12 +1,21 @@
-import { Paper } from '@mui/material'
+import { Button, Paper } from '@mui/material'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import PersistentDrawerLeft from '../components/drawer/drawer'
 import Login from '../components/login'
 export default function Desk() {
+    const router = useRouter()
+
     //MAIN LANDING PAGE, Desk name is a reference to a desk as the first workspace.
-    return(
-    <Paper elevation={18} sx={{m:'5em'}}>
-    <p>Desk Page</p>
-    <Login/>
-    <PersistentDrawerLeft></PersistentDrawerLeft>
-    </Paper>)
+    return(<>
+         <Head>
+            <title>Desk</title>
+            </Head>
+        <Paper elevation={18} sx={{m:'5em'}}>
+        <p>Desk Page</p>
+        <Button variant="contained" onClick={()=>router.push('/editor')}>GO TO EDITOR</Button>
+        <Login/>
+        <PersistentDrawerLeft></PersistentDrawerLeft>
+        </Paper>
+    </>)
 }
