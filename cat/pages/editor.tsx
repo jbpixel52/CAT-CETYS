e1402dcff78bdf77de973b4ab1e454188a759d1a
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-15 20:38:08 -0700

    started editor fields

--- cat/pages/editor.tsx
+++ cat/pages/editor.tsx
@@ -14,14 +14,15 @@ export default function Editor() {
         <Paper elevation={18} sx={{ m: '5%' }}>
             <Typography variant="h5" sx={{ m: "1em" }}>Editor</Typography>
 
-            <Stack direction="row" justifyContent={"space-evenly"} spacing={3} divider={<Divider orientation="vertical" flexItem />} alignItems="flex-start"
-
-            >
-                <Paper sx={{}}><Typography>LEFT SIDE</Typography></Paper>
-                <Card sx={{width:"15em", height:"100dvh"}}>
+            <Stack direction="row" justifyContent={"space-evenly"} spacing={3} divider={<Divider orientation="vertical" flexItem />} alignItems="flex-start">
+                <Paper sx={{}}>
+                    <Typography>LEFT SIDE</Typography>
+                    
+                </Paper>
+                <Card sx={{ width: "15em", height: "100dvh" }}>
                     <Typography>RIGHT SIDE (CARD LIVE PREVIEW)</Typography>
                     <Image src={"/syllabus_placeholder.png"} height={"100%"} width={"100%"} layout="responsive"></Image>
-                    </Card>
+                </Card>
             </Stack>
 
 
