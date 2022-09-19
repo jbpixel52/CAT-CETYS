d0300591720ecbe548bb138cde93e97d12f3e08f
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-18 23:05:52 -0700

    typo

--- cat/pages/api/dbMakerAPI/cartas/getFields.ts
+++ cat/pages/api/dbMakerAPI/cartas/getFields.ts
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         }
     } catch (error) {
-        res.status(500).json(err.message);
+        res.status(500).json(error.message);
         
     }
 
