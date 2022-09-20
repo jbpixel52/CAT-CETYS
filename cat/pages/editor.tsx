5f7c668276541b456b07566fcc58e7690f6c3613
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-20 16:41:26 -0700

    new navbar for editor

--- cat/pages/editor.tsx
+++ cat/pages/editor.tsx
@@ -5,14 +5,14 @@ import { useRouter } from "next/router";
 import theme from "../styles/theme";
 import Head from "next/head";
 import Image from "next/image";
-
+import NavBar from "../components/NavBar/navigationbar"
 export default function Editor() {
 
     return (<Box sx={{bgcolor:theme.palette.primary.light, width:'100vw', height:'100vh'}}>
-        <PersistentDrawerLeft></PersistentDrawerLeft>
         <Head>
             <title>Editor</title>
         </Head>
+        <NavBar/>
         <Paper elevation={18} sx={{ m: '5%' }}>
             <Typography variant="h5" sx={{ m: "1em" }}>Editor</Typography>
 
