52e48334b249b8d7a0c8c5cc1899e29b148e0466
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-26 21:21:31 -0700

    Some flow done

--- cat/components/login/login.tsx
+++ cat/components/login/login.tsx
@@ -1,20 +1,23 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import {Button } from "@mui/material";
+import { Button } from "@mui/material";
 import { useRouter } from 'next/router'
-
+import { useEffect, useState } from "react";
 
 export default function Login() {
-  const { data: session , status} = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
-
+  const signInParams = { callbackUrl: 'http://localhost:3000/desk' };
+  ;
   if (session) {
+    //CHANGE URL IN THE CALLBACK URL 
     return (
       <Button variant="outlined" onClick={() => {
-        signOut();
-      router.push('/')}
+        signOut({ callbackUrl: 'http://localhost:3000/' })
+      }
       }>Sign Out</Button>)
   }
   return (
-    <Button variant="contained" onClick={() => signIn("google")}>Sign in with Google</Button>
+
+    <Button variant="contained" onClick={() => signIn("google", signInParams)}>Sign in with Google</Button>
   )
 }
--- cat/pages/desk.tsx
+++ cat/pages/desk.tsx
@@ -1,11 +1,15 @@
 import {Paper, Box } from '@mui/material'
+import Button from '@mui/material/Button'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import NavBar from '../components/NavBar/navigationbar'
 import theme from '../styles/theme'
 export default function Desk() {
-    const router = useRouter()
+    const router = useRouter();
+    const { data: session, status } = useSession();
 
+    if (session) {
     //MAIN LANDING PAGE, Desk name is a reference to a desk as the first workspace.
     return(<Box sx={{bgcolor:theme.palette.primary.light, width:'100vw', height:'100vh'}}>
          <Head>
@@ -16,4 +20,6 @@ export default function Desk() {
     
         </Paper>
     </Box>)
+        
+    }
 }
