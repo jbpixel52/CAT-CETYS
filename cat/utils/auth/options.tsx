4d623ff33538f9af6b57fdbbc9d0330ea8686b9c
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-09-27 15:41:25 -0700

    gg

--- cat/utils/auth/options.tsx
+++ cat/utils/auth/options.tsx
@@ -4,20 +4,20 @@ import { NextAuthOptions } from "next-auth";
 
 
 const authOptions: NextAuthOptions = {
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-    ],
-    callbacks: {
-      async signIn({ account, profile }) {
-        if (account.provider === "google") {
-          return profile.email_verified && profile.email.endsWith("@cetys.edu.mx");
-        }
-        return true; // Do different verification for other providers that don't have `email_verified`
-      },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified && (profile.email.endsWith("@cetys.edu.mx") || profile.email.endsWith("@cetys.mx"));
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
     },
-    
+  },
+
 };
 export default authOptions;
