9022af553191b06a0d43891b66e276776d144a41
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-10-22 20:49:32 -0700

    removes unused imports

--- cat/components/search/searchbar.tsx
+++ cat/components/search/searchbar.tsx
@@ -2,11 +2,7 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import { useSession } from 'next-auth/react';
-import useSWR from 'swr';
 import { useState, useEffect } from 'react';
-import fetcher from '../../utils/fetcher';
-import { GetServerSideProps } from 'next'
-import { InferGetServerSidePropsType, InferGetStaticPropsType } from 'next/types';
 
 
 
