412ac36d6d02b00269480163a0cff5fe6bebcc91
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-10-29 22:19:03 -0700

    failed to statically generate cartas pages

--- cat/pages/api/db/cartas/get-syllabus.ts
+++ cat/pages/api/db/cartas/get-syllabus.ts
@@ -18,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 
     try {
-        if (session) {
+        if (true) {
             if (req.method === "GET") {
                 let field = await dbMakerApplication.getSyllabus(req.body)
                 res.status(200).json(field)
--- cat/pages/cartas/index.tsx
+++ cat/pages/cartas/index.tsx
@@ -1,6 +1,7 @@
 export default function cartas(){
     return(
         <div>
+            <h1 className="text-5xl">CARTAS</h1>
             </div>
     )
 }
