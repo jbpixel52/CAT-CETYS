054d20729c1deb20916f0e59054a5076ce6d1cda
hectorcanales960@gmail.com <hectorcanales960@gmail.com>
2022-11-06 21:24:35 -0800

    ozzie

--- cat/components/editor/fields.tsx
+++ cat/components/editor/fields.tsx
@@ -1,9 +1,25 @@
-
+//hardcode y luego dinamico
+//workspace => terminal => npx prisma studio para ver la info de cartas
+import { Cartas, filasCartas } from '@prisma/client';
 export default function Field(props) {
+
     function FieldItem(field) {
+
+        const fetchFilas = async (filas: string[]) => {
+            const filasData = [];
+            for await (const iterator of filas) {
+                const req: filasCartas = await fetch('http://localhost:3000/api/db/filas/getRow',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify({ rowId: iterator }),
+                    }).then(r => r.json());
+                filasData.push(<p key={req.id}>{req.filaJSON.toString()}</p>);
+        
+            }
+            return filasData;
         return (
             <div>
-
+                
             </div>
         )
     }
@@ -17,4 +33,5 @@ function FormBuilder(fields: JSON) {
 
     return (<></>);
 
+}
 }
