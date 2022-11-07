933f7df11d5e4d2e005e03290801cbdbbee6a516
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2022-11-07 15:01:51 -0800

    fixed duplicate items on cartas refresh

--- cat/pages/cartas.tsx
+++ cat/pages/cartas.tsx
@@ -7,20 +7,34 @@ import Link from 'next/link';
 
 const fetchMetadata = async () => {
     const res: Cartas[] = await fetch('http://localhost:3000/api/db/cartas/get-syllabuses').then(r => r.json());
-    //console.log(res);
     return res;
-
 }
 const postCreateSyllabus = async () => {
     return fetch('http://localhost:3000/api/db/cartas/create-syllabus', {
         method: 'POST'
     }).then(r => r.json());
+}
 
+const createBlocks = async (metadata: Cartas[]) => {
+    let listCartas = [];
+    listCartas = metadata.map((obj) => {
+        return (<div key={obj.id} className='p-1 m-2 rounded flex  gap-2 w-fit'>
+            <input type="checkbox" />
+            <button >
+                <Link href={`/carta/${obj.id}`}>
+                    <p className=' font-bold underline text-sky-600'>{obj.NOMBRE_CARTA}</p>
+                </Link>
+            </button>
+            <button>  <Link href={`/editor/${obj.id}`}>
+                <p className=' font-bold underline bg-amber-300'>EDITAR CARTA</p>
+            </Link></button>
+        </div>)
+    })
+    return listCartas;
 }
 
 export default function CartasPage() {
     const router = useRouter();
-    const [ listCartas, setlistCartas ] = useState([]);
     const [ nuevaCartaQuery, setnuevaCartaQuery ] = useState(false);
     const { isLoading, error, data } = useQuery([ 'cartas' ], () => fetchMetadata());
     const { data: nuevaCartaData, refetch } = useQuery([ 'nuevaCarta' ], () => postCreateSyllabus(),
@@ -31,34 +45,8 @@ export default function CartasPage() {
             refetchInterval: 0,
         });
 
+    const { data: blockElements, isLoading: loadingBlock, error: errorBlocks } = useQuery([ 'cartasBlocks' ], () => createBlocks(data), { enabled: !!data, })
 
-    if (data) {
-        // listCartas = (data.map((object) => {
-
-
-
-        // }
-        // )
-        // )
-
-        data.forEach((obj) => {
-            listCartas.push(
-                <div key={obj.id} className='p-1 m-2 rounded flex  gap-2 w-fit'>
-                    <input type="checkbox" />
-                    <button >
-                        <Link href={`/carta/${obj.id}`}>
-                            <p className=' font-bold underline text-sky-600'>{obj.NOMBRE_CARTA}</p>
-                        </Link>
-                    </button>
-                    <button>  <Link href={`/editor/${obj.id}`}>
-                        <p className=' font-bold underline bg-amber-300'>EDITAR CARTA</p>
-                    </Link></button>
-
-                </div>
-            )
-
-        })
-    }
 
     return (
         <div className="flex justify-between flex-col">
@@ -67,7 +55,8 @@ export default function CartasPage() {
                 <h1 className="text-5xl">CARTAS</h1>
 
                 <div className='bg-amber-200 w-fit'>
-                    {listCartas}
+                    {loadingBlock ? "cargando lista de cartas..." :
+                        errorBlocks ? 'error cargando cartas' : blockElements ? blockElements : null}
                 </div>
 
 
