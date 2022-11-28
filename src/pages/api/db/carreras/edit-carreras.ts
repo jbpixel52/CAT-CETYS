81143460e79c6b9f489c9bfc2aa7bcec4447bb6f
Omar Duran <54329113+OktaOEDC@users.noreply.github.com>
2022-11-28 15:44:54 -0800

    Edit and Get
    Only 1 document in collection, no creation endpoint

--- src/pages/api/db/carreras/edit-carreras.ts
+++ src/pages/api/db/carreras/edit-carreras.ts
@@ -15,7 +15,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             if (req.method === 'PATCH') {
                 try {
                     console.log('finding carrera list')
-                    const carreras: any = await prisma.nombresCarrera.findMany({});
+                    const carreras: any = await prisma.nombresCarrera.findFirst({
+                        where: {
+                            id: {
+                                equals: req.body?.id
+                            }
+                        }
+                    });
+                    const update = await prisma.nombresCarrera.update({
+                        where: {
+                            id: carreras.id
+                        },
+                        data: {
+                            NOMBRES: {
+                                push: req.body
+                            }
+                        }
+                    })
                 } catch (error) { res.status(400).json(error); }
             } else { res.status(400).json("Este endpoint es solo para solicitudes PATCH para algo de nombres de carreras") }
         } else { res.status(401).json("Unathourized access.") }
